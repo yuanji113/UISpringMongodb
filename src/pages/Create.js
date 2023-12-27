eb15aea4784bacf8811f61ef336a41dc4fe891c9
Yuan Ji <yuanji1996@gmail.com>
2023-12-27 17:35:40 -0500

    change port number

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -32,7 +32,7 @@ const Create = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch("http://localhost:8080/post", {
+    fetch("http://localhost:8082/post", {
       method: "POST", // or 'PUT'
       headers: {
         "Content-Type": "application/json",
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -19,11 +19,11 @@ const Feed = () => {
   //
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await axios.get(`http://localhost:8080/posts/${query}`);
+      const response = await axios.get(`http://localhost:8082/posts/${query}`);
       setPost(response.data);
     };
     const fetchInitialPosts = async () => {
-        const response = await axios.get(`http://localhost:8080/allPosts`);
+        const response = await axios.get(`http://localhost:8082/allPosts`);
         console.log(response);
         setPost(response.data);
     }
